83af1b5d01442f3e45fec58bb95faad98eaa455e
Rifqi Aditya <rifqiadityaa24@gmail.com>
2024-08-10 19:14:52 +0700

    chore: fix redirect

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,15 @@ const nextConfig = {
         hostname: "images.unsplash.com",
       },
     ],
+    redirects: async () => {
+      return [
+        {
+          source: "/",
+          destination: "/home",
+          permanent: true,
+        },
+      ];
+    },
   },
 };
 
