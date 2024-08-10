288164f8e56a524625ae679fc1f27a8d6de6960f
Rifqi Aditya <rifqiadityaa24@gmail.com>
2024-08-10 19:58:33 +0700

    feat: create nav drawer for mobile

---
+++ src/components/Navbar/Drawer/index.tsx
@@ -0,0 +1,45 @@
+import Button from "@/components/Button";
+import { FC } from "react";
+import { DrawerProps } from "./types";
+
+const Drawer: FC<DrawerProps> = ({ items, open, setDrawerOpen }) => {
+  return (
+    <div
+      className={`flex justify-center items-center fixed z-[999] inset-y-0 right-0 transition-all duration-300 bg-[#004040] ${
+        open ? "w-full" : "w-0"
+      }`}
+    >
+      {open && (
+        <Button
+          text="x"
+          variant="icon"
+          className="border-white text-white absolute top-6 right-6"
+          onClick={() => {
+            setDrawerOpen(false);
+          }}
+        />
+      )}
+      <ul
+        className={`flex flex-col text-center items-center justify-center gap-6 ${
+          open ? "" : "hidden"
+        }`}
+      >
+        {items.map((item, index) => (
+          <a
+            key={index}
+            href={item.link}
+            className="text-base font-bold text-white px-6 py-4 border-b-2 border-transparent hover:border-white transition-all ease-in-out duration-300"
+          >
+            {item.label}
+          </a>
+        ))}
+        <Button
+          text="Need Assistance?"
+          className={"hover:bg-[#D6B66B] hover:border-[#D6B66B]"}
+        />
+      </ul>
+    </div>
+  );
+};
+
+export default Drawer;
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -4,9 +4,11 @@ import { FC, useEffect, useState } from "react";
 import Button from "../Button";
 import HamburgerMenu from "../DynamicAssets/HamburgerMenu";
 import ZamroodLogo from "../DynamicAssets/ZamroodLogo";
+import Drawer from "./Drawer";
 
 const Navbar: FC<NavbarProps> = ({ items, logo }) => {
   const [navBoxVisible, setNavBoxVisible] = useState(false);
+  const [isMobileDrawerOpen, setIsMobileDrawerOpen] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -25,34 +27,46 @@ const Navbar: FC<NavbarProps> = ({ items, logo }) => {
   }, []);
 
   return (
-    <nav
-      className={`flex justify-between fixed w-full p-4 xl:px-44 py-6 z-50 max-xl:bg-white transition-all ease-in-out duration-500 ${
-        navBoxVisible ? "xl:bg-[#004040]" : "xl:bg-transparent"
-      }`}
-    >
-      {logo || <ZamroodLogo />}
-      <div className="max-[1400px]:hidden flex">
-        <ul className="flex gap-6 ">
-          {items.map((item, index) => (
-            <a
-              key={index}
-              href={item.link}
-              className="text-base font-bold text-white px-6 py-4 border-b-2 border-transparent hover:border-white transition-all ease-in-out duration-300"
-            >
-              {item.label}
-            </a>
-          ))}
-          <Button
-            text="Need Assistance?"
-            className={`${
-              navBoxVisible ? "hover:bg-[#D6B66B] hover:border-[#D6B66B]" : ""
-            }`}
-          />
-        </ul>
-      </div>
+    <>
+      <Drawer
+        open={isMobileDrawerOpen}
+        setDrawerOpen={setIsMobileDrawerOpen}
+        items={items}
+      />
+      <nav
+        className={`flex justify-between fixed w-full p-4 xl:px-44 py-6 z-50 max-xl:bg-white transition-all ease-in-out duration-500 ${
+          navBoxVisible ? "xl:bg-[#004040]" : "xl:bg-transparent"
+        }`}
+      >
+        {logo || <ZamroodLogo />}
+        <div className="max-[1400px]:hidden flex">
+          <ul className="flex gap-6 ">
+            {items.map((item, index) => (
+              <a
+                key={index}
+                href={item.link}
+                className="text-base font-bold text-white px-6 py-4 border-b-2 border-transparent hover:border-white transition-all ease-in-out duration-300"
+              >
+                {item.label}
+              </a>
+            ))}
+            <Button
+              text="Need Assistance?"
+              className={`${
+                navBoxVisible ? "hover:bg-[#D6B66B] hover:border-[#D6B66B]" : ""
+              }`}
+            />
+          </ul>
+        </div>
 
-      <div className="min-[1400px]:hidden">{logo || <HamburgerMenu />}</div>
-    </nav>
+        <div
+          className="min-[1400px]:hidden"
+          onClick={() => setIsMobileDrawerOpen(true)}
+        >
+          {logo || <HamburgerMenu />}
+        </div>
+      </nav>
+    </>
   );
 };
 
